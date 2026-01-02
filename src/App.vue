4ff6174e912435036b35b7920970d47c3e91ca95
Filippo Comelato <filippocomelato19@gmail.com>
2026-01-02 17:02:14 +0100

    Implement hero section

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <script setup>
+import Hero from "./components/Hero.vue";
 import Navbar from "./components/Navbar.vue";
 </script>
 
 <template>
   <main>
     <Navbar />
+    <Hero />
   </main>
 </template>
 
---
+++ src/components/Hero.vue
@@ -0,0 +1,22 @@
+<script setup>
+import { onMounted, ref } from "vue";
+
+const videoRef = ref(null);
+
+onMounted(() => {});
+</script>
+
+<template>
+  <section id="hero">
+    <div>
+      <h1>MacBook Pro</h1>
+      <img src="/title.png" alt="MacBook Title" />
+    </div>
+
+    <video :ref="videoRef" src="/videos/hero.mp4" autoplay muted playsinline />
+    <button>Buy</button>
+    <p>From $1599 or $133/mo for 12 months</p>
+  </section>
+</template>
+
+<style scoped></style>
